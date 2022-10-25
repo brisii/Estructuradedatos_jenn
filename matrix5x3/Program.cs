bb36092f99a740b2947bc60d1c1cf6e0620a17ba
BJimenez <jjimenezu@miumg.edu.gt>
2022-10-25 15:18:58 -0700

    jenny

---
+++ matrix5x3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Matriz
+{
+    class Matriz
+    {
+        private int[,] mat;
+
+        public void Ingresar()
+        {
+            mat = new int[5, 3];
+            for (int f = 0; f < 5; f++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Console.Write("Ingrese valor de  posicion ["+(f+1)+","+(c+1)+"]: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    mat[f, c] = int.Parse(linea);
+                }
+            }
+        }
+
+        public void Imprimir()
+        {
+            for (int f = 0; f < 5; f++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Console.Write(mat[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Matriz ma = new Matriz();
+            ma.Ingresar();
+            ma.Imprimir();
+        }
+    }
+}
---
+++ operario/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operario
+{
+    class operario
+    {
+        private int[] sueldos;
+
+        public void Cargar()
+        {
+            sueldos = new int[5];
+            for (int f = 0; f < 5; f++)
+            {
+                Console.Write("Ingrese el sueldo del operario:");
+                String linea;
+                linea = Console.ReadLine();
+                sueldos[f] = int.Parse(linea);
+            }
+        }
+    
+        public void Imprimir() 
+        {
+            for(int f = 0; f < 5; f++) 
+            {
+                Console.WriteLine(sueldos[f]);
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            operario pv = new operario();
+            pv.Cargar();
+            pv.Imprimir();
+        }
+    }
+}
